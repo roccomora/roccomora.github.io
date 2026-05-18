9a0cca1d133016d87d120fdec25315217bacf30d
Rocco <rocco.mora@lirmm.fr>
2026-05-18 15:42:44 +0200

    2026/05/18

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -52,8 +52,8 @@ let transTheme = () => {
 let initTheme = (theme) => {
   if (theme == null || theme == 'null') {
     const userPref = window.matchMedia;
-    if (userPref && (userPref('(prefers-color-scheme: dark)').matches || userPref('(max-width: 768px)').matches || true)) {
-        theme = 'dark';
+    if (userPref && (userPref('(prefers-color-scheme: light)').matches || userPref('(max-width: 768px)').matches || true)) {
+        theme = 'light';
     }
   }
   
