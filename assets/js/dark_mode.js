b3e2b34531f18f5e0bf713b9a3418a827964e384
Rocco <rocco.mora@lirmm.fr>
2026-05-27 16:10:59 +0200

    2026/05/27

--- assets/js/dark_mode.js
+++ assets/js/dark_mode.js
@@ -1,8 +1,12 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const mode_toggle = document.getElementById("light-toggle");
 
-    mode_toggle.addEventListener("click", function() {
-        toggleTheme(localStorage.getItem("theme"));
+    mode_toggle.addEventListener("click", function () {
+        const current = document.documentElement.getAttribute("data-theme");
+
+        const next = current === "dark" ? "light" : "dark";
+
+        document.documentElement.setAttribute("data-theme", next);
+        localStorage.setItem("theme", next);
     });
 });
-
